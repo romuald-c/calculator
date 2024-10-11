abb96625afad83caaacf1f72c661fc618485b641
romuald-c <romuald.gorda@gmail.com>
2024-10-11 17:50:53 +0100

    Calculator Summary
    First small summary

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator calculator = new Calculator();
+            Console.WriteLine("Hello Romuald!");
+        }
+    }
+}
